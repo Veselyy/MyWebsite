c62912f652d65d9fee5fbb0a0be5225345a6ebfb
martinvesely <imthemartinveselyap@gmail.com>
2025-04-23 20:54:51 +0200

    Rozdělání sekce se vzděláním apod.

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+$("#hs").click(function(){
+    $("#hs-modal-box").css("display", "flex").animate({ opacity: 1 }, 300).addClass('modal-active');
+    $("body").css("overflow", "hidden");
+});
+
+$("#hs-modal-box").click(function(){
+    $("#hs-modal-box").animate({ opacity: 0 }, 300, function() {
+        $(this).css("display", "none").removeClass('modal-active');
+    });
+    $("body").css("overflow", "auto");
+});
+
+
+$("#college").click(function(){
+    $("#college-modal-box").css("display", "flex").animate({ opacity: 1 }, 300).addClass('modal-active');
+    $("body").css("overflow", "hidden");
+});
+
+$("#college-modal-box").click(function(){
+    $("#college-modal-box").animate({ opacity: 0 }, 300, function() {
+        $(this).css("display", "none").removeClass('modal-active');
+    });
+    $("body").css("overflow", "auto");
+});
+
+
+$("#brigade").click(function(){
+    $("#commity-modal-box").css("display", "flex").animate({ opacity: 1 }, 300).addClass('modal-active');
+    $("body").css("overflow", "hidden");
+});
+
+$("#commity-modal-box").click(function(){
+    $("#commity-modal-box").animate({ opacity: 0 }, 300, function() {
+        $(this).css("display", "none").removeClass('modal-active');
+    });
+    $("body").css("overflow", "auto");
+});
+
+
+
+$(".modal-block").click(function (event) {
+    event.stopPropagation();
+});
